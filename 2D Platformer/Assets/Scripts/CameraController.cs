96b32734e5a32d6dd85e4419f3dded8dc0a991f9
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-08 08:01:50 +0200

    2D Platformer - Camera Controller

---
+++ 2D Platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Transform farBackground, middleBackground;
+
+    public float minHeight, maxHeight;
+    public float minWidth, maxWidth;
+
+    private Vector2 lastPosistion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosistion = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Camera follows targeted player with restrictions
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, minWidth, maxWidth), Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z);
+
+        // Background parallax depth ( moving background )
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPosistion.x, transform.position.y - lastPosistion.y);
+
+        farBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f);
+        middleBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f) * .5f;
+
+        lastPosistion = transform.position;
+    }
+}
