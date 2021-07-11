1c9a4c02236c33a7fcb27b4d13ebf4f545494144
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-11 14:04:39 +0200

    2D Platformer - Enemy Animation

--- 2D Platformer/Assets/Scripts/EnemyController.cs
+++ 2D Platformer/Assets/Scripts/EnemyController.cs
@@ -11,44 +11,74 @@ public class EnemyController : MonoBehaviour
     private bool movingRight;
 
     private Rigidbody2D theRB;
-
+    private Animator anim;
     public SpriteRenderer theSR;
+
+    public float moveTime, waitTime;
+    private float moveCount, waitCount;
 
     // Start is called before the first frame update
     void Start()
     {
         theRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
         // Enemy frog no longer parent, because of moving logic
         leftPoint.parent = null;
         rightPoint.parent = null;
 
         movingRight = true;
+
+        moveCount = moveTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(movingRight)
-        {
-            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        if (moveCount > 0) {
 
-            theSR.flipX = true;
+            moveCount -= Time.deltaTime;
 
-            if(transform.position.x > rightPoint.position.x)
+            if (movingRight)
             {
-                movingRight = false;
-            }
-        } else
-        {
-            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+                theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
 
-            theSR.flipX = false;
+                theSR.flipX = true;
 
-            if (transform.position.x < leftPoint.position.x)
+                if (transform.position.x > rightPoint.position.x)
+                {
+                    movingRight = false;
+                }
+            } else
             {
-                movingRight = true;
+                theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+
+                theSR.flipX = false;
+
+                if (transform.position.x < leftPoint.position.x)
+                {
+                    movingRight = true;
+                }
             }
+
+            if (moveCount <= 0)
+            {
+                waitCount = Random.Range(waitTime * .75f, waitTime * 1.25f);
+            }
+
+            anim.SetBool("isMoving", true);
+        } else if (waitCount > 0)
+        {
+            waitCount -= Time.deltaTime;
+
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+
+            if (waitCount <= 0)
+            {
+                moveCount = Random.Range(moveTime * .75f, moveTime * 1.25f);
+            }
+
+            anim.SetBool("isMoving", false);
         }
     }
 }
