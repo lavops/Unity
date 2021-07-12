928449c78e82b2aa3277c822ff36b1a2739aa62b
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-12 10:05:26 +0200

    2D Platformer - Audio Mixer & Audio Pitch

--- 2D Platformer/Assets/Scripts/AudioManager.cs
+++ 2D Platformer/Assets/Scripts/AudioManager.cs
@@ -8,6 +8,8 @@ public class AudioManager : MonoBehaviour
 
     public AudioSource[] soundEffects;
 
+    public AudioSource backgroundMusic, levelEndMusic;
+
     private void Awake()
     {
         instance = this;   
@@ -28,6 +30,7 @@ public class AudioManager : MonoBehaviour
     public void PlaySFX(int soundToPlay)
     {
         soundEffects[soundToPlay].Stop();
+        soundEffects[soundToPlay].pitch = Random.Range(.9f, 1.1f);
         soundEffects[soundToPlay].Play();
     }
 }
