3ae1c00b6b7e4fbe4d993f58a089d92a6dd803f5
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-12 15:02:27 +0200

    2D Platformer - Fadeout when dying and respawning

--- 2D Platformer/Assets/Scripts/LevelManager.cs
+++ 2D Platformer/Assets/Scripts/LevelManager.cs
@@ -38,7 +38,13 @@ public class LevelManager : MonoBehaviour
 
         PlayerController.instance.gameObject.SetActive(false);
 
-        yield return new WaitForSeconds(waitToRespawn);
+        yield return new WaitForSeconds(waitToRespawn - (1f / UIController.instance.fadeSpeed));
+
+        UIController.instance.FadeToBlack();
+
+        yield return new WaitForSeconds((1f / UIController.instance.fadeSpeed) + .2f);
+
+        UIController.instance.FadeFromBlack();
 
         PlayerController.instance.gameObject.SetActive(true);
 
--- 2D Platformer/Assets/Scripts/UIController.cs
+++ 2D Platformer/Assets/Scripts/UIController.cs
@@ -13,6 +13,10 @@ public class UIController : MonoBehaviour
 
     public Text gemCount;
 
+    public Image fadeScreen;
+    public float fadeSpeed;
+    private bool shouldFadeToBlack, shouldFadeFromBlack;
+
     private void Awake()
     {
         instance = this;
@@ -22,12 +26,31 @@ public class UIController : MonoBehaviour
     void Start()
     {
         UpdateGemDisplay();
+        FadeFromBlack();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(shouldFadeToBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
+
+            if (fadeScreen.color.a == 1f)
+            {
+                shouldFadeToBlack = false;
+            }
+        }
         
+        if(shouldFadeFromBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+
+            if (fadeScreen.color.a == 0f)
+            {
+                shouldFadeFromBlack = false;
+            }
+        }
     }
 
     public void UpdateHealthDisplay()
@@ -76,5 +99,17 @@ public class UIController : MonoBehaviour
     public void UpdateGemDisplay()
     {
         gemCount.text = LevelManager.instance.gemsCollected.ToString();
+    }
+
+    public void FadeToBlack()
+    {
+        shouldFadeToBlack = true;
+        shouldFadeFromBlack = false;
+    }
+
+    public void FadeFromBlack()
+    {
+        shouldFadeFromBlack = true;
+        shouldFadeToBlack = false;
     }
 }
