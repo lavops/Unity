5bc6e45905fbbfa0fa82920c5adc3472fd869594
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-14 12:09:23 +0200

    2D Platformer - Moving Platform

---
+++ 2D Platformer/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform[] points;
+
+    public Transform platform;
+
+    public float moveSpeed;
+
+    public int currentPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        platform.position = Vector3.MoveTowards(platform.position, points[currentPoint].position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(platform.position, points[currentPoint].position) < .05f)
+        {
+            currentPoint++;
+
+            if(currentPoint >= points.Length)
+            {
+                currentPoint = 0;
+            }
+        }
+    }
+}
