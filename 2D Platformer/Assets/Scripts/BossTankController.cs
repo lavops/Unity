0b07253baa83a726afcfd2afc54f6402f7163075
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-17 07:31:07 +0200

    2D Platformer - Boss Tank Controller

---
+++ 2D Platformer/Assets/Scripts/BossTankController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTankController : MonoBehaviour
+{
+    public Transform theBoss;
+    public Animator anim;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public Transform leftPoint, rightPoint;
+    private bool moveRight;
+
+    [Header("Shooting")]
+    public GameObject bullet;
+    public Transform firePoint;
+    public float timeBetweenShots;
+    private float shotsCounter;
+
+    [Header("Hurt")]
+    public float hurtTime;
+    private float hurtCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
