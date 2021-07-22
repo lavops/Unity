7d51e3a72f3f5aac1abc29424415050249fcbd1b
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-07-22 08:54:39 +0200

    2D Platformer - Boss States

--- 2D Platformer/Assets/Scripts/BossTankController.cs
+++ 2D Platformer/Assets/Scripts/BossTankController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BossTankController : MonoBehaviour
 {
+    public enum bossStates { shooting, hurt, moving };
+    public bossStates currentState;
+
     public Transform theBoss;
     public Animator anim;
 
@@ -24,12 +27,49 @@ public class BossTankController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = bossStates.shooting;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentState) 
+        {
+            case bossStates.shooting:
+
+                break;
+
+            case bossStates.hurt:
+                if(hurtCounter > 0) 
+                {
+                    hurtCounter -= Time.deltaTime;
+
+                    if(hurtCounter <= 0)
+                    {
+                        currentState = bossStates.moving;
+                    }
+                }
+
+                break;
+
+            case bossStates.moving:
+
+                break;
+        }
+
+#if UNITY_EDITOR
+
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            TakeHit();
+        }
+
+#endif
+    }
+
+    public void TakeHit()
+    {
+        currentState = bossStates.hurt;
+        hurtCounter = hurtTime;
     }
 }
